5adc43c540f7fd2527aec120afbc63b979e7258d
Joe Diragi <joyod3@gmail.com>
2022-07-28 13:21:42 -0400

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Joe Diragi on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
